f19ab0a2ca2d08211148fb2433cd33429ff1562e
FabricioVegaAlfaro <fabriciovega0123@gmail.com>
2025-08-19 08:40:38 -0600

    Fix API base

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://examenes-app-354o.onrender.com",
   withCredentials: false, // No necesitamos cookies en este caso
   headers: { "Content-Type": "application/json" },
 });
